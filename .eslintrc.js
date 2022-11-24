48932e58a2634ee2f09a009469811095f3c26d1d
Daniel Cooke <danielcooke1996@gmail.com>
2022-11-24 20:58:53 +0000

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,20 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
+  extends: ['plugin:react/recommended', 'standard'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-  }
-}
+    semi: 'off',
+    'comma-dangle': 'off',
+  },
+};
--- src/core.tsx
+++ src/core.tsx
@@ -1,2 +1,2 @@
-export * from './hooks'
-export * from './server'
+export * from './hooks';
+export * from './server';
