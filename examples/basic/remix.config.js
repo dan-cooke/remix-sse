9aba9c1c69a467211d33c1fc9bf701a38fe685c5
Daniel Cooke <danielcooke1996@gmail.com>
2023-11-23 12:30:08 +0100

    chore: add examples cypress test
    * cypress is erroring when opening, it is saying the config file is incorrect

--- examples/basic/remix.config.js
+++ examples/basic/remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
+
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
