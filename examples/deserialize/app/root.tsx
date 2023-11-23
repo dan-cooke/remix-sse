d1d642ec0cdab412973346539057583ac976f5d7
Daniel Cooke <danielcooke1996@gmail.com>
2023-11-23 10:10:33 +0100

    chore: update deserialize example for v2
    chore: rename deserialize-v2 folder

--- examples/deserialize/app/root.tsx
+++ examples/deserialize/app/root.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from '@remix-run/node';
+import { cssBundleHref } from "@remix-run/css-bundle";
+import { RemixSseProvider } from 'remix-sse/dist/client/index.js'
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -6,25 +8,24 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from '@remix-run/react';
-import { RemixSseProvider } from '../../../src/client/RemixSseProvider';
+} from "@remix-run/react";
 
-export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'New Remix App',
-  viewport: 'width=device-width,initial-scale=1',
-});
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
 
 export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
         <RemixSseProvider>
-          <Outlet />
+        <Outlet />
         </RemixSseProvider>
         <ScrollRestoration />
         <Scripts />
